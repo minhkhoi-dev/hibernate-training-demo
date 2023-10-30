ffcea889e1bfa976b06738da49769bffc7d6080d
Khoi <khoi@MK-MacBook-Pro.local>
2023-10-30 15:50:54 +0700

    Add demo cache query

--- src/main/java/fa/training/Main.java
+++ src/main/java/fa/training/Main.java
@@ -1,12 +1,9 @@
 package fa.training;
 
 import fa.training.entity.Department;
-import fa.training.entity.Employee;
 import org.hibernate.Session;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-
-import java.util.HashMap;
 
 public class Main {
   public static void main(String[] args) {
@@ -15,29 +12,30 @@ public class Main {
         .configure("hibernate.cfg.xml")
         .build();
          var sessionFactory = new MetadataSources(registry)
-             .addAnnotatedClass(Department.class)
-                 .addAnnotatedClass(Employee.class)
              .buildMetadata().buildSessionFactory()) {
 
       sessionFactory.inTransaction(Main::doWithSession);
+      System.out.println("---------------------------------");
       sessionFactory.inTransaction(Main::doWithSession);
+
       // Clear cache
       sessionFactory.getCache().evictAllRegions();
       System.out.println("Cache cleared\n");
+      System.out.println("---------------------------------");
+      System.out.printf("Cache exist for: id=%s : %s\n", 2,
+              sessionFactory.getCache().contains(Department.class, 2));
     }
   }
 
   private static void doWithSession(Session session) {
 
     // Do your logic here
-    var deps = new HashMap<Integer, Department>();
+    var result = session.createQuery("select d from Department d where d.id = :id", Department.class)
+            .setParameter("id", 2)
+            .setCacheable(true)
+            .getSingleResult();
 
-    for (int i = 0; i < 5; i++) {
-      var dep = session.find(Department.class, i + 1);
-      deps.put(i+1, dep);
-    }
-
-    deps.forEach((key, dep) ->  System.out.printf("Cache exist for: %s : %s\n",
-        dep, session.getSessionFactory().getCache().contains(Department.class, key)));
+    System.out.printf("Cache exist for query: id=%s: %s : %s\n", 2, result,
+            session.getSessionFactory().getCache().contains(Department.class, 2));
   }
 }
