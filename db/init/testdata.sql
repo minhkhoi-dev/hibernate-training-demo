0e0a0496cb46143835f06f591672298b9fbc66dd
Khoi <khoi@MK-MacBook-Pro.local>
2023-10-30 11:39:44 +0700

    Update demo to use MSSQL

--- db/init/testdata.sql
+++ db/init/testdata.sql
@@ -1,14 +1,14 @@
-drop table if exists public.departments;
-create table public.departments
+--drop table dbo.departments;
+create table dbo.departments
 (
     dept_id   integer not null
         primary key,
     dept_name varchar(255)
 );
 
-INSERT INTO public.departments (dept_id, dept_name) VALUES (1, 'Test department 1');
-INSERT INTO public.departments (dept_id, dept_name) VALUES (2, 'Test department 2');
-INSERT INTO public.departments (dept_id, dept_name) VALUES (3, 'Test department 3');
-INSERT INTO public.departments (dept_id, dept_name) VALUES (4, 'Test department 4');
-INSERT INTO public.departments (dept_id, dept_name) VALUES (5, 'Test department 5');
+INSERT INTO dbo.departments (dept_id, dept_name) VALUES (1, 'Test department 1');
+INSERT INTO dbo.departments (dept_id, dept_name) VALUES (2, 'Test department 2');
+INSERT INTO dbo.departments (dept_id, dept_name) VALUES (3, 'Test department 3');
+INSERT INTO dbo.departments (dept_id, dept_name) VALUES (4, 'Test department 4');
+INSERT INTO dbo.departments (dept_id, dept_name) VALUES (5, 'Test department 5');
 
